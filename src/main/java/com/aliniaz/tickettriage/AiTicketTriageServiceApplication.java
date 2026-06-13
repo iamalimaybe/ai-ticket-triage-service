15d6b66f3f8914f42b54b89e9c6e8ac3056ab697
iamalimaybe <52281468+iamalimaybe@users.noreply.github.com>
2026-06-13 11:21:08 +0500

    Add Spring Boot application skeleton

---
+++ src/main/java/com/aliniaz/tickettriage/AiTicketTriageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aliniaz.tickettriage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTicketTriageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTicketTriageServiceApplication.class, args);
+	}
+
+}
